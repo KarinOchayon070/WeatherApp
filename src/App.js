779053747066ff5b69a4c2c28f5fb5f2cb91c2a7
Karin Ochayon <karinoch070@gmail.com>
2023-08-08 12:22:51 +0300

    Updating the app & deleting files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Jerusalem&appid=98aeda6895903a5368aa4716a632db79';
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='app'>
+
+    <div className='container'>
+
+      <div className='top'>
+        <div className='location'>
+          <p>Jerusalem</p>
+        </div>
+        <div className='temp'>
+          <h1>40°C</h1>
+        </div>
+        <div className='description'>
+          <p>hot</p>
+        </div>
+      </div>
+
+      <div className='bottom'>
+        <div className='feels'>
+          <p>45°C</p>
+        </div>
+        <div className='humidity'>
+          <p>20%</p>
+        </div>
+        <div className='wind'>
+          <p>5 MPH</p>
+        </div>
+
+      </div>
+    </div>
     </div>
   );
 }
